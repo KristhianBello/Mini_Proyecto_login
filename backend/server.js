0223f478d79de6c9110cf7c899891a6c4fc71a21
Kristhian Bello <kristhianbello12@gmail.com>
2025-05-30 11:06:56 -0500

    funca

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // Servir archivos estáticos del frontend
 
 app.get('/ping', (req, res) => {
